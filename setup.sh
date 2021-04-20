b1d8fc72446914f8cd638048389da8effeba5ec0
Abdel <abdelrazak.sakhri@gmail.com>
2021-04-20 16:52:47 +0200

    rectification de fichier

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
-mkdir -p ~/.streamlit
-echo "[server]
-headless = true
-enableCORS=false
-port = $PORT
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
